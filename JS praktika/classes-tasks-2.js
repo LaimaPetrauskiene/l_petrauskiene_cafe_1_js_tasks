c564288c1bdeb4902c0225acf6cd35ab6a7650f8
Laima Petrauskiene <laima@tinklai.net>
2021-10-11 12:05:30 +0300

    some class tasks done

--- JS praktika/classes-tasks-2.js
+++ JS praktika/classes-tasks-2.js
@@ -67,19 +67,17 @@ console.groupCollapsed('4. - https://edabit.com/challenge/iwdZiFucR5wkQsFHu');
       this.age = age;
     }
 
-    compareAge(other) {
-      // Write code here!
 
+    compareAge(other) {
+      if (this.age < other.age) {
+        return `${other.name} is older than me.`
+      } else if (other.age < this.age) {
+        return `${other.name} is younger than me.`
+      } else {
+        return `${other.name} is the same age as me.`
+      }
     }
   }
-
-  p1.compareAge(p2) ➞ "Joel is older than me."
-
-  p2.compareAge(p1) ➞ "Samuel is younger than me."
-
-  p1.compareAge(p3) ➞ "Lily is the same age as me."
-
-
 }
 console.groupEnd();
 
@@ -98,26 +96,48 @@ console.groupEnd();
 console.groupCollapsed('6. - https://edabit.com/challenge/qNMtrtizgssAQqP2b');
 {
   class Name {
-    constructor(fname, lname){
+    constructor(fname, lname) {
       this.fname = fname.charAt(0).toUpperCase() + fname.slice(1).toLowerCase();
       this.lname = lname.charAt(0).toUpperCase() + lname.slice(1).toLowerCase();
       this.fullname = this.fname + ' ' + this.lname;
-      this.initials = fname.slice(0,1).toUpperCase() + '.' + lname.slice(0,1).toUpperCase();
+      this.initials = fname.slice(0, 1).toUpperCase() + '.' + lname.slice(0, 1).toUpperCase();
     }
-    
+
   }
 }
 console.groupEnd();
 
 console.groupCollapsed('7. - https://edabit.com/challenge/HKmJFmZZCX53ff4ke');
 {
-  // ... code
+  function sweetestIcecream(arr) {
+
+    let max = 0;
+    for (let i = 0; i < arr.length; i++)
+      max = Math.max(max, flavor[`${arr[i].flavor}`] + arr[i].numSprinkles);
+    return max;
+  }
+
+  let flavor = {
+    Plain: 0,
+    Vanilla: 5,
+    ChocolateChip: 5,
+    Strawberry: 10,
+    Chocolate: 10
+  }
+
 }
 console.groupEnd();
 
 console.groupCollapsed('8. - https://edabit.com/challenge/9zwdrfW99zmdRhibi');
 {
-  // ... code
+  class OnesThreesNines {
+    constructor(n) {
+      this.ones = Math.floor(n / 1);
+      this.threes = Math.floor(n / 3);
+      this.nines = Math.floor(n / 9)
+    }
+  }
+
 }
 console.groupEnd();
 
@@ -130,15 +150,15 @@ console.groupEnd();
 console.groupCollapsed('10. - https://edabit.com/challenge/s5Sz2ovKsvtGxNGgn');
 {
   class Book {
-    constructor(title, author){
+    constructor(title, author) {
       this.title = title;
       this.author = author;
-  }	
-     getTitle() {return `Title: ${this.title}`;}
-    
-     getAuthor() {return `Author: ${this.author}`;}
+    }
+    getTitle() { return `Title: ${this.title}`; }
+
+    getAuthor() { return `Author: ${this.author}`; }
   }
-  
+
   let PP = new Book('Pride and Prejudice', 'Jane Austen');
   let H = new Book('Hamlet', 'William Shakespeare');
   let WP = new Book('War and Peace', 'Leo Tolstoy');
